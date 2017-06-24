e4e6a31596194763abcb5766c43febb7f3f5a4cb
Archangel354 <rduncan354@yahoo.com>
2017-06-24 16:22:46 -0400

    Added 3 addional Fragments and got them to display; ParksFragment, MuseumFragment and BeachesFragment.  I also did wrap_content on the height of the lower textView (Address) in order that the bottom text wouldn't get cut off.

--- app/src/main/java/com/example/owner/tourguide/MuseumsFragment.java
+++ app/src/main/java/com/example/owner/tourguide/MuseumsFragment.java
@@ -6,7 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,9 +24,22 @@ public class MuseumsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
-    }
 
+
+        View rootView = inflater.inflate(R.layout.attraction_list, container, false);
+
+
+        final ArrayList<Attraction> attractions = new ArrayList<Attraction>();
+        attractions.add(new Attraction("Trip's Diner", "2339 Dr Martin Luther King Jr St North", R.drawable.tripsdiner_restaurant));
+        attractions.add(new Attraction("Red Mesa", "4912 4th St North", R.drawable.redmesa_restaurant));
+        attractions.add(new Attraction("Noble Crust", "8300 4th St North", R.drawable.noblecrust_restaurant));
+        attractions.add(new Attraction("Birch & Vine", "340 Beach Dr NE", R.drawable.birchandvine_restaurant));
+
+        AttractionAdapter adapter = new AttractionAdapter(getActivity(), attractions, R.color.category_museums);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+        listView.setAdapter(adapter);
+
+        return rootView;
+    }
 }
